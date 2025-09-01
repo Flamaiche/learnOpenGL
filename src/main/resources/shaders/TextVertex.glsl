69f3e37c13ec2a7b824790096dece29457be02e8
Flamaiche <Flamaiche@gmail.com>
2025-09-01 08:39:44 +0200

    text good

--- src/main/resources/shaders/TextVertex.glsl
+++ src/main/resources/shaders/TextVertex.glsl
@@ -1,11 +1,10 @@
 #version 330 core
-layout(location = 0) in vec2 vertex;
+layout(location = 0) in vec2 position;
 
 uniform mat4 projection;
 uniform vec2 offset;
 uniform float scale;
 
 void main() {
-    vec2 pos = vertex * scale + offset;
-    gl_Position = projection * vec4(pos, 0.0, 1.0);
+    gl_Position = projection * vec4(position * scale + offset, 0.0, 1.0);
 }
